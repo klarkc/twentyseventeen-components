b735abb31184e84692161272cb9b57a68b77123d
Walker <walker@praiseweb.com.br>
2018-03-30 19:52:10 -0300

    split logic into functions

--- components/loader.php
+++ components/loader.php
@@ -1,25 +1,79 @@
 <?php
 define("LOADER_URI", get_template_directory_uri() . '/' . basename(__DIR__));
 
-$loadedTemplates = array();
+$loadedModules = array();
+
+$dependencies = array(
+    'https://unpkg.com/@webcomponents/webcomponentsjs@1.1.1/webcomponents-loader.js' => false,
+    'https://unpkg.com/vue@2.5.16/dist/vue.js' => false,
+    'https://unpkg.com/@vue/web-component-wrapper@1.2.0/dist/vue-wc-wrapper.global.js' => false,
+);
+
+function components_loader_get_register($module) {
+    ob_start();
+    ?>
+    <script>
+        window.addEventListener('WebComponentsReady', function() {
+            // At this point we are guaranteed that all required polyfills have loaded,
+            // all HTML imports have loaded, and all defined custom elements have upgraded
+            let ElementClass = customElements.get('<?php echo $module; ?>');
+            let element = document.querySelector('<?php echo $module; ?>');
+            console.assert(element instanceof ElementClass);  // 👍
+        });
+    </script>
+    <?
+    return ob_end_clean();
+}
+
+function components_loader_get_dependencies() {
+    $output = '';
+    $scriptTmpl = '<script type="text/javascript" src="%s"></script>';
+    foreach($dependencies as $dep => $loaded) {
+        if ($loaded === true) continue;
+        $output .= sprintf($scriptTmpl, $dep);
+        $dependencies[$dep] = true;
+    }
+
+    return $output;
+}
+
+function components_loader_get_module($src) {
+    $moduleTmpl = '<script type="module" src="%s"></script>';
+    return sprintf($moduleTmpl, esc_url($src));
+}
+
+function components_loader_get_template($src, $handle) {
+    $themePath = get_template_directory();
+    $themePath = str_replace(ABSPATH, '', $themePath);
+    $scriptPath = parse_url($src);
+    $scriptName = basename($scriptPath['path']);
+    $tmplPath = str_replace($scriptName, 'template.html', $scriptPath['path']);
+    $tmplPath = str_replace($themePath, '', $tmplPath);
+    array_push($loadedModules, $handle);
+    return require($themePath . $tmplPath);
+}
 
 function components_loader_format_script_tag($output, $handle, $src) {
-    global $loadedTemplates;
+    global $loadedModules;
 
-    // if path is for components folder
-    if (strpos($src, LOADER_URI) !== false) {
-        $scriptTmpl = '<script type="module" src="%s"></script>';
-        $output = sprintf($scriptTmpl, esc_url($src));
-        if (!in_array($handle, $loadedTemplates)) {
-            $themePath = get_template_directory();
-            $themePath = str_replace(ABSPATH, '', $themePath);
-            $scriptPath = parse_url($src);
-            $scriptName = basename($scriptPath['path']);
-            $tmplPath = str_replace($scriptName, 'template.html', $scriptPath['path']);
-            $tmplPath = str_replace($themePath, '', $tmplPath);
-            $output .= "\n\n" . require($themePath . $tmplPath);
-            array_push($loadedTemplates, $handle);
-        }
+    // must load if path is for components folder
+    // and components is still not loaded
+    $mustLoad = (
+        strpos($src, LOADER_URI) !== false &&
+        !in_array($handle, $loadedModules)
+    );
+    if ($mustLoad) {
+        // erase loading script
+        $output = '';
+
+        // load global dependencies
+        $output .= components_loader_get_dependencies();
+
+        // load module
+        $output .= components_loader_get_module($src);
+
+        // load template
+        $output .= components_loader_get_template($src, $handle);
     }
     return $output;
 }
